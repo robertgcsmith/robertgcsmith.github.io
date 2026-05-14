810fbb2e1f6568317890e7f60f6380399a4f1bc6
robertgcsmith <robertgcsmith@gmail.com>
2026-05-14 11:32:28 +0000

    Deploying to gh-pages from @ robertgcsmith/robertgcsmith.github.io@206397d62f583f59e9031099eeae6a119a253a9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,17 +429,17 @@ ninja.data = [{
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/projects/7_project.md/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/projects/8_project.md/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/9_project.md/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
