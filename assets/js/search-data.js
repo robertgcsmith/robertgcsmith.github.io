1269cf5b76dabb14b7050ccb7a38d113696a3ed5
robertgcsmith <robertgcsmith@gmail.com>
2026-08-27 11:50:31 +0000

    Deploying to gh-pages from @ robertgcsmith/robertgcsmith.github.io@4a887f55accc43812758103880dc1ed9443667b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "Projects",
-          description: "A selection of current projects",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
